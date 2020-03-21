4e255e7fa4835aa8a1fe39decd16b2dd90236b82
Victor <60289708+VictorBr79@users.noreply.github.com>
2020-03-21 19:22:13 +0100

    Add files via upload

---
+++ Porte monnaie.py
@@ -0,0 +1,135 @@
+import os
+
+
+def main():
+    print("Avant de commencer je vais vous expliquer les règles du jeu."
+          "Le but du jeu c'est de finir le jeu sans etre pauvre."
+          "Vous allez devoir entrer des prix (chiffres) mais vous ne pourrez pas entrer des nombres a virgule."
+          "Viola c'est parti!")
+    os.system("PAUSE")
+    print("Dans votre porte monnaie vous avez actuellement 1000 euro!")
+    prm = int(input("Vous entrez dans le H&M, combiez depansez vous? : "))
+    if prm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = 1000 - prm
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    dxm = int(input("Vous entrez maintenant dans le Media Markt. Combien depensez vous? : "))
+    if dxm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - dxm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    trm = int(input("Vous entrez maintenant dans le Delhaize. Combien depensez vous? : "))
+    if trm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - trm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    qtm = int(input("Vous allez maintenant manger dans un restaurant. Combien depensez vous? : "))
+    if qtm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - qtm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    sqm = int(input("Vous entrez maintenant dans une boulangerie. Combien depensez vous? : "))
+    if sqm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - sqm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    print("Jour de paye! Vous recevez votre salaire qui est de 500 euro.")
+    prt = prt + 500
+    print("vous avez maintenant " + str(prt) + " euro.")
+    sxm = int(input("Vous entrez dans un night shop, combiez depansez vous? : "))
+    if sxm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - sxm
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    stm = int(input("Vous entrez maintenant dans le Colruyt. Combien depensez vous? : "))
+    if stm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - stm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    htm = int(input("Vous entrez maintenant chez Samsung. Combien depensez vous? : "))
+    if htm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - htm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    nvm = int(input("Vous allez maintenant manger au McDonald's. Combien depensez vous? : "))
+    if nvm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - nvm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    dxm = int(input("Vous allez maintenant chez un docteur. Combien depensez vous? : "))
+    if dxm <= 0:
+        print("Vous n'avez rien depensez. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    if prt <= 0:
+        print("Vous n'avez plus d'argent. Vous ètes mort.")
+        os.system("PAUSE")
+        exit()
+    prt = prt - dxm
+    print("Il ne vous reste plus que " + str(prt) + " euro.")
+    print("Jour de paye! Vous recevez votre salaire qui est de 500 euro.")
+    prt = prt + 500
+    print("vous avez maintenant " + str(prt) + " euro.")
+    print("Vous avez réussi le jeu sans etre pauvre!")
+    print("Merci d'avoir jouer!")
+
+
+if __name__ == '__main__':
+    main()
+
+
+os.system("PAUSE")
